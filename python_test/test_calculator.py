c1b297dd900979f65146a4b8e12d2b095dc5beca
karthikeyan-web <1912014@nec.edu.in>
2026-08-08 12:18:03 +0530

    initial commit

---
+++ python_test/test_calculator.py
@@ -0,0 +1,12 @@
+from calculator import add, subtract;
+
+def test_add():
+    assert add(2,3) == 5;
+    assert add(-1,1) == 0;
+    assert add(1,1) == 2;
+
+def test_subtract():
+    assert subtract(2,3) == 1;
+    assert subtract(1,1) == 0;
+    assert subtract(10,5) == 5;
+
